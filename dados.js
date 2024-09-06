8e10a8e40ad5583b83d23819e2a0647376110c53
Ketley Gonçalves <97256278+ketleygoncalves@users.noreply.github.com>
2024-09-06 12:37:36 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,68 @@
+let dados = [
+  {
+      titulo: "Ada Lovelace",
+      descricao: "Considerada a primeira programadora, Ada Lovelace contribuiu significativamente para o desenvolvimento dos primeiros computadores e algoritmos.",
+      link: "https://pt.wikipedia.org/wiki/Ada_Lovelace",
+      tags: "programação algoritmos pioneira"
+  },
+  {
+      titulo: "Carol Shaw",
+      descricao: "Uma das primeiras mulheres a trabalhar como designer de jogos eletrônicos, Carol Shaw foi pioneira em um campo que se tornaria uma das maiores indústrias de entretenimento do mundo.",
+      link: "https://en.wikipedia.org/wiki/Carol_Shaw",
+      tags: "jogos eletrônicos design pioneira"
+  },
+  {
+      titulo: "Evelyn Boyd Granville",
+      descricao: "A terceira mulher afro-americana a obter um Ph.D. em matemática nos Estados Unidos, Evelyn Boyd Granville contribuiu significativamente para a área da computação.",
+      link: "https://en.wikipedia.org/wiki/Evelyn_Boyd_Granville",
+      tags: "matemática computação pioneira"
+  },
+  {
+      titulo: "Frances Allen",
+      descricao: "Cientista da computação, Frances Allen foi a primeira mulher a ganhar o Prêmio Turing, considerado o 'Nobel da computação'. Seus trabalhos em otimização de compiladores e análise de programas tiveram um impacto duradouro na indústria.",
+      link: "https://en.wikipedia.org/wiki/Frances_Allen",
+      tags: "computação otimização pioneira"
+  },
+  {
+      titulo: "Grace Hopper",
+      descricao: "Grace Hopper foi uma pioneira da computação, desenvolvedora da linguagem de programação COBOL e conhecida por popularizar o termo 'bug'.",
+      link: "https://pt.wikipedia.org/wiki/Grace_Hopper",
+      tags: "COBOL bug pioneira"
+  },
+  {
+      titulo: "Hedy Lamarr",
+      descricao: "Além de ser uma atriz famosa, Hedy Lamarr co-inventou um sistema de comunicação secreta que é a base da tecnologia wireless moderna.",
+      link: "https://pt.wikipedia.org/wiki/Hedy_Lamarr",
+      tags: "wireless comunicação pioneira"
+  },
+  {
+      titulo: "Jean Bartik",
+      descricao: "Uma das ENIAC Girls, Jean Bartik foi uma das primeiras programadoras de computadores eletrônicos, ajudando a moldar o futuro da computação.",
+      link: "https://en.wikipedia.org/wiki/Jean_Bartik",
+      tags: "ENIAC programadora pioneira"
+  },
+  {
+      titulo: "Katherine Johnson",
+      descricao: "Matemática brilhante, Katherine Johnson fez cálculos cruciais para as missões espaciais da NASA, incluindo os voos de Alan Shepard e John Glenn.",
+      link: "https://en.wikipedia.org/wiki/Katherine_Johnson",
+      tags: "matemática NASA pioneira"
+  },
+  {
+      titulo: "Radia Perlman",
+      descricao: "Engenheira de software, Radia Perlman é conhecida como a 'mãe da internet'. Ela inventou o protocolo Spanning Tree Protocol (STP), fundamental para a estabilidade e funcionamento das redes de computadores.",
+      link: "https://en.wikipedia.org/wiki/Radia_Perlman",
+      tags: "internet STP pioneira"
+  },
+  {
+      titulo: "Rafaella Ballerini",
+      descricao: "Criadora de conteúdo de programação e tecnologia, fundadora de uma comunidade de mais de 40 mil membros: A Comunidade Ballerini",
+      link: "https://www.linkedin.com/in/rafaellaballerini/",
+      tags: "programação tecnologia comunidade"
+  },
+  {
+      titulo: "Silvia Coelho",
+      descricao: "Engenheira, Palestrante, Fundadora da Elas Programam. Pós-graduanda em Comunicação Quântica e LinkedIn Top Voice & LinkedIn Creator",
+      link: "https://www.linkedin.com/in/silvia-coelho/",
+      tags: "engenharia palestrante comunicação"
+  }
+];
